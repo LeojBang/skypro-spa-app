6ec7f3c19d1a1dc9f61b14caf8117c02fe72d797
maksim <m.merkulovdodo@gmail.com>
2025-05-08 21:28:42 +0300

    homework 32_1 done

--- lms/serializer.py
+++ lms/serializer.py
@@ -1,19 +1,34 @@
 from rest_framework.serializers import ModelSerializer, SerializerMethodField
 
 from lms.models import Course, Lesson
+from lms.validators import YouTubeUrlValidator
+from lms.models import Subscription
 
 
 class CourseSerializer(ModelSerializer):
+    is_subscribed = SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            return Subscription.objects.filter(
+                user=request.user,
+                course=obj
+            ).exists()
+        return False
 
 
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = "__all__"
-
+        validators = [
+            YouTubeUrlValidator(field='video_url'),
+        ]
 
 class CourseDetailSerializer(ModelSerializer):
     lesson_count = SerializerMethodField()
@@ -25,3 +40,4 @@ class CourseDetailSerializer(ModelSerializer):
     class Meta:
         model = Course
         fields = ("name", "preview", "description", "lesson_count", "lessons")
+
